8e637d21dfd7202ff47b530eb1ecb4ee42ec079d
Raj Rana <be2020se715@gces.edu.np>
2026-04-27 18:28:51 +0545

    Day 2

--- Phase 1/day1/Hello.java
+++ Phase 1/day1/Hello.java
@@ -1,3 +1,4 @@
+package day1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ Phase 1/day2/IfElseDemo.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+       for( int i=0;i<=20;i++){
+        if(i%2==0){
+            System.out.println(i+ " is Even" );
+        }else{
+            System.out.println(i+" is Odd");
+        }
+       }
+    }
+}
