bbe5ef46aa383e6507ebbfcd16c46ae98e4845d3
Raj Rana <be2020se715@gces.edu.np>
2026-05-27 00:59:38 +0545

    Anagram

--- Randoms/Leetcode/Anagram.java
+++ Randoms/Leetcode/Anagram.java
@@ -6,17 +6,22 @@ import java.util.Map;
 public class Anagram {
 
     public boolean isAnagram(String s, String t){
-        if(s.length() != t.length()){
+        if(s.length()!=t.length()){
             return false;
         }
 
         Map<Character, Integer> count = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             char charS = s.charAt(i);
-            char charT = t.charAt(i);
+            char charT = s.charAt(i);
 
+            count.put(charS, count.getOrDefault(charS,0)+1);
+            count.put(charT, count.getOrDefault(charT, 0)-1);
+        }
 
+        for(int val: count.values()){
+            if (val != 0) return false;
         }
         return true;
     }
